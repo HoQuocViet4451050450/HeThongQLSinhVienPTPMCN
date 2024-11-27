7c00945fae107fd0310a92b47eb10abaca21e983
HUY NGUYEN <huycongan65@gmail.com>
2024-11-27 16:47:23 +0700

    chuc nang dang xuat

---
+++ ASPSTUDENT/Controllers/DangXuatController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPSTUDENT.Controllers
+{
+    public class DangXuatController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            // Đăng xuất khỏi hệ thống
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // Điều hướng về trang chủ (hoặc trang đăng nhập)
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
